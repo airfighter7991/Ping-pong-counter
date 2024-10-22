38a8faaa8915845f8f47580db5f6eca3abdc0b3f
Artemiy <artemiy@email.com>
2024-10-22 18:43:11 +0300

    Added new features

--- js/script.js
+++ js/script.js
@@ -6,10 +6,27 @@ l.innerHTML = 0
 var r = document.getElementById("rght");
 r.innerHTML = 0
 
+var al = document.getElementById("wind");
+var cpt = document.getElementById("caption");
+valid = true;
+
 function inc(s){
     if (s==="l"){
         l.innerHTML++
+        
     }else{
         r.innerHTML++
     }
+    if(l.innerHTML>=21 && valid){
+        cpt.innerHTML = "Левый игрок победил."
+        al.style.display = "block";
+    }
+    if(r.innerHTML>=21 && valid){
+        cpt.innerHTML = "Правый игрок победил."
+        al.style.display = "block";
+    }
+}
+function cl(){
+    al.style.display = "none";
+    valid= false;
 }
